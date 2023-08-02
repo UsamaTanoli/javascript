f27b5cd1b473ae954c37c02d7779b7ffa9704c59
UsamaTanoli <usamakhan123451998@gmail.com>
2023-08-02 15:42:19 +0500

    Primitive data type

---
+++ 02_dataType.js
@@ -0,0 +1,19 @@
+// dataTypes in js
+
+// Primitive Types
+
+// String => "any thing between double or single quotes"
+// Number => 1,2 etc
+// Bigint => whole numbers larger than 253-1
+// Boolean => true, false
+// Undefined => any variable which is not defined eg: let name;
+// Null => no value;
+// Symbol
+
+console.log(typeof "shaban")
+console.log(typeof true);
+console.log(typeof 15)
+console.log(typeof undefined)
+console.log(typeof null)  // this will through "object" in type
+console.log(typeof Symbol("anas"))
+console.log(typeof 9007199254740992n);
