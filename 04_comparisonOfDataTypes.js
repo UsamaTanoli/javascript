d6d2f69aeedfd84404bcd3771b104fb889902bb4
UsamaTanoli <usamakhan123451998@gmail.com>
2023-08-08 17:14:05 +0500

    comparison of data types

---
+++ 04_comparisonOfDataTypes.js
@@ -0,0 +1,9 @@
+//**************** must avoid this type of cpmparison*/
+
+// console.log(null <= 0)
+// console.log(null >= 0)
+// console.log(null > 0); // false
+// console.log(null < 0); //
+// console.log(true >= 1)
+
+// console.log(undefined == 0)
