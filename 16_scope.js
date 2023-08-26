7a0b0e8cb34c92038cd9d02acf60149008f02a25
UsamaTanoli <usamakhan123451998@gmail.com>
2023-08-26 14:30:07 +0500

    scope in js

--- 16_scope.js
+++ 16_scope.js
@@ -3,21 +3,28 @@ var a = 11;
 let b = 22;
 const c = 33;
 
-// we can call a,b,c in any local scope because these have global scope 
 
 console.log(a);
 console.log(b);
 console.log(c);
+// we can call a,b,c in any local scope {} because these have global scope 
+// {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
 
 console.log("after block scope");
+{
+
+}
 // what if we putt values in block scope
 {
     // var a = 12;
     // let b = 23;
     // const c = 34;
     // we can not call b and c outside {} because thay have local scope except a because var = "global scope"
-    console.log(b);
-    console.log(c);
+
  }
 // console.log(a); // here you see just a will change because var has global scope
 // console.log(b);
