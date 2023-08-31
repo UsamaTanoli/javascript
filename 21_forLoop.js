60b8cefaf59b0d97bd62581509e2640f5f15e826
UsamaTanoli <usamakhan123451998@gmail.com>
2023-09-01 01:56:37 +0500

    for loop with break and continue in js

---
+++ 21_forLoop.js
@@ -0,0 +1,57 @@
+// For Loop
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log("this is",element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("here comes 5");
+//     }
+//     console.log("this is",element);
+// }
+
+// for (let i = 1; i <=5 ; i++) {
+//       console.log(`
+//     "${i}" table start`);
+//     for (let j = 1; j <= 10; j++) {
+        
+//         console.log(`${i} × ${j} = ${i*j}`);
+        
+        
+//     };
+    
+// }
+
+// let arr = ["cat", "dog", "lion"];
+
+// for (let i = 0; i < arr.length; i++){
+//     let e = e[i];
+//     console.log(e);
+// }
+
+// this can also be weite like this
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+
+// let nameArr = ["anas", "shaban", "usama", "okasha", "bilal"];
+
+// for (let i = 0; i < nameArr.length; i++){
+//     if(nameArr[i] === "usama"){
+//         console.log(`"${nameArr[i]}" the lion detected on index ${i} so please abbort mission`);
+//         break
+//     }
+//     console.log(nameArr[i]);
+// }
+
+// for (let i = 0; i < nameArr.length; i++){
+//     if(nameArr[i] === "usama"){
+//         console.log(`"${nameArr[i]}" the lion detected on index ${i} so please abbort mission`);
+//         continue
+//     }
+//     console.log(nameArr[i]);
+// }
