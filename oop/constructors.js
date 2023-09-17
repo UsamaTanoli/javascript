9765bb2e879a17fc39ed131c3fa287b2f27b6fe1
UsamaTanoli <usamakhan123451998@gmail.com>
2023-09-17 18:35:45 +0500

    consturctor functions in js

---
+++ oop/constructors.js
@@ -0,0 +1,48 @@
+// // let cars = {
+// //     carName: "ford f-150",
+// //     model: 2022,
+// //     color: "white"
+// // }
+
+// // cars.cAr = function () {
+// //     return (`this is cars name ${this.carName}`)
+// //     // return this
+// // }
+// // console.log(cars.cAr());
+
+
+// // let user = {
+// //     name: "usama",
+// //     age: 34,
+// //     profession: "blockChain developer",
+
+// //     method: function () {
+// //         // return `this is the method of ${this.name}`
+// //         // return this
+// //     }
+// // }
+
+// // console.log(user.name);
+// // console.log(user.method());
+
+// function User(name, cast, gender){
+//     this.name = name
+//     this.cast = cast
+//     this.gender = gender
+//     this.greeting = function() {
+//         return `hello ${this.name} good morning`
+//     }
+   
+    
+// }
+
+// let value1 = new User('usama', "tanoli", "male")
+// let value2 = new User("anas", "tanoli", "male")
+
+// console.log(value1);
+// console.log(value2);
+// console.log(value1.greeting());
+// console.log(value2.greeting());
+// console.log(value1 instanceof User);
+// console.log(User.prototype);
+
